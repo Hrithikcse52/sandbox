bb0d022e662f28573cdb8f43452a400abfc2c81f
hrithikcse52 <hrithik.cse52@gmail.com>
2023-03-19 17:59:24 +0530

    chore: project structure update

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+import { IndexPage } from "./page";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <IndexPage />,
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
-
-
---
+++ client/src/page/index.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Button } from "../components";
+
+export const IndexPage = () => {
+  const [value, setValue] = useState("");
+  useEffect(() => {
+    const fetch = async () => {
+      const { data } = await axios.get("http://localhost:3333");
+      console.log("dataL:", data);
+      setValue(data?.message ?? "something went wrong!!");
+    };
+    fetch();
+  }, []);
+  return (
+    <div>
+      helloasd {value}
+      <h1>asdas</h1>
+      <Button />
+    </div>
+  );
+};
