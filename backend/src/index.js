c54349a4ae889d6829f7de017b8ddecbae8842df
hrithikcse52 <hrithik.cse52@gmail.com>
2023-03-19 18:04:36 +0530

    chore: added dot env

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-
+const port = process.env.PORT ?? 3333;
 app.use(cors());
 
 app.get("/", (req, res) => {
+  console.log("env:", process.env.PORT);
   return res.send({ message: "hello aayush" });
 });
 
-app.listen(3333, () => {
+app.listen(port, () => {
   console.log("server is up and running");
 });
